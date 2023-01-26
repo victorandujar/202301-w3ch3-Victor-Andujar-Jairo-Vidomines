2ceb76dc318970824c58c12237b98840f89d27fc
Jairo Vidomines <jvidomines@gmail.com>
2023-01-26 20:19:55 +0100

    Add elements to main component

--- src/components/Components/MainComponent/MainComponent.ts
+++ src/components/Components/MainComponent/MainComponent.ts
@@ -10,6 +10,9 @@ export class MainComponent extends Component {
 
     this.element.innerHTML = `
       <h2 class="main-content__title">Series list</h2>
+      <section class="list">
+        <h3 class="list__title">Pending series</h3>
+        <span class="list__info">You have 4 series pending to watch</span>
     `;
   }
 }
