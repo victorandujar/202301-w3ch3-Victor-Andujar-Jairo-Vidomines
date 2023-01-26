0067315bb68621515bb43718df418285ea799009
Jairo Vidomines <jvidomines@gmail.com>
2023-01-26 19:46:44 +0100

    Create main component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { HeaderComponent } from "./components/HeaderComponent/HeaderComponent";
+import { HeaderComponent } from "./components/Components/HeaderComponent/HeaderComponent";
+import { MainComponent } from "./components/Components/MainComponent/MainComponent";
 
 const container = document.querySelector(".container")!;
-
 const header = new HeaderComponent(container);
-
 header.render();
+
+const main = new MainComponent(container);
+main.render();
